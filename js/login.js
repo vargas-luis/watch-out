4416854fa470e5c11aed23fe24e8eaa4b3e07b9a
Luis Vargas <pilotoasado@outlook.com>
2022-11-15 11:08:15 -0600

    Login & prodcutos

---
+++ js/login.js
@@ -0,0 +1,39 @@
+function cleanScreen(){
+    document.getElementById('relojes').style.display = "none";
+    document.getElementById('cronografos').style.display = "none";
+    document.getElementById('correas').style.display = "none";
+    document.getElementById('login__field').style.display = "contents";    
+    
+    document.getElementById('login__correo').value="";
+    document.getElementById('login__pass').value="";
+    
+    document.getElementById('login__correo').focus();
+    
+}
+
+function showScreen(){
+    document.getElementById('relojes').style.display = "contents";
+    document.getElementById('cronografos').style.display = "contents";
+    document.getElementById('correas').style.display = "contents";
+    document.getElementById('login__field').style.display = "none";  
+}
+
+function login(){
+    var usuario = "alura@mail.com";
+    var passwor = "alura321";
+
+    var inicioIngresado;
+    var passworIngresado;
+
+    inicioIngresado = document.getElementById('login__correo').value;
+    passworIngresado= document.getElementById('login__pass').value;
+        
+    if( inicioIngresado == usuario && passworIngresado == passwor ) {
+        alert("Bienvenido al sistema " + inicioIngresado);
+    } else {
+        alert("Inicio de sesión inválido.");
+    }
+
+    showScreen();
+}
+
--- readme.md
+++ readme.md
@@ -1,9 +1,22 @@
-#Tienda Watch Out!
+# Tienda Watch Out!
 -------------------------
+## Noviembre 14, 2022
 Tienda de Relojes creada para el programa de Oracle Next Education, impartido por Alura.
 Creada con HTML5, CSS y Javascript.
+
+Datos de acceso:
+> Correo electrónico: alura@mail.com
+> Contraseña: alura321
 
 Dale un vistazo en:
 
 <a href="https://vargas-luis.github.io/watch-out/" target="_blank">https://vargas-luis.github.io/watch-out/</a>
 
+
+## Noviembre 15, 2022
+- Catálogo de productos completo
+- Login creado, con los siguientes datos de acceso:
+  - alura@mail.com
+  - alura321
+- Falta diseño para dispositivos móviles
+- Contacto aún no funcional
